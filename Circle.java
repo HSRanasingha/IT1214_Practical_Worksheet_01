610b4e54dac6c35cbed05737cec06fba55dfbd8b
H S Ranasingha <harshanisadunika99@gmail.com>
2025-05-20 17:43:31 +0000

    main

---
+++ Circle.java
@@ -0,0 +1,13 @@
+public class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    void area() {
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of Circle: " + area);
+    }
+}
